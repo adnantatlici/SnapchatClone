277b9592035e4738bf3d0bf11241f35b90c8d79f
Mustafa Adnan Tatlıcı <mustafaadnantatlici@Mustafa-Air.modem.local>
2023-02-14 02:55:08 +0300

    first commit

---
+++ SnapchatClone/SignInVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Mustafa Adnan Tatlıcı on 13.02.2023.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
